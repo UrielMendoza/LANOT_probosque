b0348737c8f097e72e2ce1f576c42871b01bcf4d
UrielMendoza <46738566+UrielMendoza@users.noreply.github.com>
2022-10-03 14:10:39 -0500

    Update probosque_dndvi.py

--- probosque_dndvi.py
+++ probosque_dndvi.py
@@ -18,21 +18,19 @@ for line in lines:
     files.sort()
     for fileMeta,file in zip(filesMeta,files):
         print('Porcesando: '+file)
+        print('Meta: '+fileMeta)
 
         meta = minidom.parse(fileMeta)
         radioSF_b6 = meta.getElementsByTagName('ps:radiometricScaleFactor')[5].firstChild.data
         radioSF_b8 = meta.getElementsByTagName('ps:radiometricScaleFactor')[7].firstChild.data
         refleSF_b6 = meta.getElementsByTagName('ps:reflectanceCoefficient')[5].firstChild.data
         refleSF_b8 = meta.getElementsByTagName('ps:reflectanceCoefficient')[7].firstChild.data
-        print(radioSF_b6)
-        print(radioSF_b8)
-        print(refleSF_b6)
-        print(refleSF_b8)
+
+        print(type(radioSF_b6))
 
         ds = rasterio.open(file)
         band6 = ds.read(6)
         band8 = ds.read(8)
-
 
         print(band6)
         print(band8)
