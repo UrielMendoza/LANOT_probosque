43f41a43f33de7904bd27be234261718fd20709c
UrielMendoza <46738566+UrielMendoza@users.noreply.github.com>
2022-10-03 13:59:29 -0500

    Update probosque_dndvi.py

--- probosque_dndvi.py
+++ probosque_dndvi.py
@@ -2,13 +2,14 @@ import funtions
 from glob import glob
 import rasterio 
 import numpy as np
+from xml.dom import minidom
 
 pathInputPlanet = '/data/input/probosque/PLANET2022/'
 pathOutput = '/data/output/probosque/'
 
 lines = glob(pathInputPlanet+'*')
 
-print(lines)
+#print(lines)
 
 for line in lines:
     filesMeta = glob(line+'/'+'*_metadata.xml')
@@ -16,12 +17,18 @@ for line in lines:
     filesMeta.sort()
     files.sort()
     for fileMeta,file in zip(filesMeta,files):
-        print(fileMeta)
-        print(file)
+        print('Porcesando: '+file)
+
+        meta = minidom.parse(fileMeta)
+        radioSF = file.getElementsByTagName('ps:radiometricScaleFactor')[6].firstChild.data
+        refleSF = file.getElementsByTagName('ps:reflectanceCoefficient')[8].firstChild.data
+        print(radioSF)
+        print(refleSF)
 
         ds = rasterio.open(file)
         band6 = ds.read(6)
         band8 = ds.read(8)
+
 
         print(band6)
         print(band8)
