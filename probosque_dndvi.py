cb026c0981c6efaf2fc4da8157960f10f9970db1
UrielMendoza <46738566+UrielMendoza@users.noreply.github.com>
2022-10-03 13:50:19 -0500

    Update probosque_dndvi.py

--- probosque_dndvi.py
+++ probosque_dndvi.py
@@ -11,14 +11,21 @@ lines = glob(pathInputPlanet+'*')
 print(lines)
 
 for line in lines:
+    filesMeta = glob(line+'/'+'*_metadata.xml')
     files = glob(line+'/'+'*_harmonized.tif')
-    for file in files:
+    filesMeta.sort()
+    files.sort()
+    for fileMeta,file in zip(filesMeta,files):
+        print(fileMeta)
+        print(file)
+
         ds = rasterio.open(file)
         band6 = ds.read(6)
         band8 = ds.read(8)
 
         print(band6)
         print(band8)
+        print(ds.width, ds.height)
 
 
 
