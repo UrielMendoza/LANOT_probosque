b99afe0d9353a289b16c0b07f5597619d8161337
UrielMendoza <urielmendozacastillo@gmail.com>
2022-10-03 18:27:10 -0500

    line dir

---
+++ planet_spot_dndvi.py
@@ -0,0 +1,55 @@
+import funtions
+from glob import glob
+import rasterio 
+import numpy as np
+from xml.dom import minidom
+import os
+
+pathInputPlanet = '/data/output/probosque/planet_ndvi/'
+pathOutput = '/data/output/probosque/planet_ndvi/'
+
+lines = glob(pathInputPlanet+'*')
+
+#print(lines)
+
+for line in lines:
+    filesMeta = glob(line+'/'+'*_metadata.xml')
+    files = glob(line+'/'+'*_harmonized.tif')
+    filesMeta.sort()
+    files.sort()
+    for fileMeta,file in zip(filesMeta,files):
+        print('Porcesando: '+file)
+        print('Meta: '+fileMeta)
+
+        meta = minidom.parse(fileMeta)
+        radioSF_b6 = float(meta.getElementsByTagName('ps:radiometricScaleFactor')[5].firstChild.data)
+        radioSF_b8 = float(meta.getElementsByTagName('ps:radiometricScaleFactor')[7].firstChild.data)
+        refleSF_b6 = float(meta.getElementsByTagName('ps:reflectanceCoefficient')[5].firstChild.data)
+        refleSF_b8 = float(meta.getElementsByTagName('ps:reflectanceCoefficient')[7].firstChild.data)
+
+        ds = rasterio.open(file)
+        b6 = ds.read(6) * radioSF_b6
+        b8 = ds.read(8) * radioSF_b8
+        ndvi = (b8 - b6) / (b8 + b6)
+
+        print(ndvi)
+        print(ds.width, ds.height)
+
+        kwargs = ds.meta
+        kwargs.update(
+            dtype=rasterio.float32,
+            count=1,
+            compress='lzw')
+
+        lineDir = line.split('/')[-1]
+        os.system('mkdir '+pathOutput+lineDir) 
+        name = file.split('/')[-1].split('.')[0]+'_ndvi.tif'
+
+        with rasterio.open(os.path.join(pathOutput+lineDir, name), 'w', **kwargs) as dst:
+            dst.write_band(1, ndvi.astype(rasterio.float32))
+
+
+
+
+
+
