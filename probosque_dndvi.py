7168fccc8f85b4e2d1f8e515d4f4544bcb2d107c
UrielMendoza <46738566+UrielMendoza@users.noreply.github.com>
2022-10-03 12:00:18 -0500

    Update probosque_dndvi.py

--- probosque_dndvi.py
+++ probosque_dndvi.py
@@ -4,10 +4,12 @@ from glob import glob
 pathInputPlanet = '/data/input/probosque/PLANET2022/'
 pathOutput = ''
 
-files = glob(pathInputPlanet+'*_harmonized.tif')
+lines = glob(pathInputPlanet+'*')
 
-for file in files:
-    print(file)
+for line in lines:
+    files = glob(pathInputPlanet+'*_harmonized.tif')
+    for file in files:
+        print(file)
 
 
 
