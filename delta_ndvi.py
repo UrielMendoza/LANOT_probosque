56168ed943a8f9588bd958394391ebe046e74be3
UrielMendoza <urielmendozacastillo@gmail.com>
2022-10-08 12:16:38 -0500

    Update delta_ndvi.py

--- delta_ndvi.py
+++ delta_ndvi.py
@@ -22,57 +22,63 @@ for lineSpot, linePlanet in zip(linesSpot, linesPlanet):
     filesSpot.sort()
     filesPlanet.sort()
 
-    for fileSpot, filePlanet in zip(filesSpot, filesPlanet):
-        print('Procesando: '+fileSpot)
-        print('Procesando: '+filePlanet)
+    if lineSpot.split('/') == 'L7':
 
-        ds_spot = rasterio.open(fileSpot)
-        ds_planet = rasterio.open(filePlanet)
+        for fileSpot, filePlanet in zip(filesSpot, filesPlanet):
+            print('Procesando: '+fileSpot)
+            print('Procesando: '+filePlanet)
 
-        ndvi_spot = ds_spot.read(1) 
-        ndvi_planet = ds_planet.read(1)
-        
-        print(ndvi_spot)
-        print(ndvi_planet)
+            ds_spot = rasterio.open(fileSpot)
+            ds_planet = rasterio.open(filePlanet)
 
-        dndvi = ndvi_spot - ndvi_planet 
-        dndvi_std = np.nanstd(dndvi)
-        dndvi_mean = np.nanmean(dndvi)
-
-        print('Delta NDVI')
-        print(dndvi)
-        print('Desviacion estandar')
-        print(dndvi_std)
-        print('Media')
-        print(dndvi_mean)
-
-        kwargs = ds_planet.meta
-        kwargs.update(
-            dtype=rasterio.float32,
-            count=1,
-            compress='lzw')      
-
-        lineDir = lineSpot.split('/')[-1]
-
-        os.system('mkdir '+pathOutput+lineDir) 
-        name = fileSpot.split('/')[-1].split('.')[0]+'_dndvi.tif'
-
-        with rasterio.open(os.path.join(pathOutput+lineDir, name), 'w', **kwargs) as dst:
-            dst.write_band(1, dndvi.astype(rasterio.float32))
-
-        dndvi_class = dndvi.copy()
-        
-        os.system('mkdir '+pathOutputClass+lineDir) 
-
-        for i in range(1,4):
-            dndvi_class[np.where(dndvi_class < (dndvi_mean - dndvi_std*i))] = -1
-            dndvi_class[np.where((dndvi_class >= (dndvi_mean - dndvi_std*i)) & (dndvi_class <= (dndvi_mean + dndvi_std*i)))] = 0
-            dndvi_class[np.where(dndvi_class > (dndvi_mean + dndvi_std*i))] = 1
+            ndvi_spot = ds_spot.read(1) 
+            ndvi_planet = ds_planet.read(1)
             
-            name = fileSpot.split('/')[-1].split('.')[0]+'_class_'+str(i)+'sd_dndvi.tif'
+            print(ndvi_spot)
+            print(ndvi_planet)
 
-            with rasterio.open(os.path.join(pathOutputClass+lineDir, name), 'w', **kwargs) as dst:
-                dst.write_band(1, dndvi_class.astype(rasterio.float32))
+            dndvi = ndvi_spot - ndvi_planet 
+            dndvi_std = np.nanstd(dndvi)
+            dndvi_mean = np.nanmean(dndvi)
+
+            print('Delta NDVI')
+            print(dndvi)
+            print('Desviacion estandar')
+            print(dndvi_std)
+            print('Media')
+            print(dndvi_mean)
+
+            kwargs = ds_planet.meta
+            kwargs.update(
+                dtype=rasterio.float32,
+                count=1,
+                compress='lzw')      
+
+            lineDir = lineSpot.split('/')[-1]
+
+            os.system('mkdir '+pathOutput+lineDir) 
+            name = fileSpot.split('/')[-1].split('.')[0]+'_dndvi.tif'
+
+            with rasterio.open(os.path.join(pathOutput+lineDir, name), 'w', **kwargs) as dst:
+                dst.write_band(1, dndvi.astype(rasterio.float32))
+
+            dndvi_class = dndvi.copy()
+            
+            os.system('mkdir '+pathOutputClass+lineDir) 
+
+            for i in range(1,4):
+                dndvi_class[np.where(dndvi_class < (dndvi_mean - dndvi_std*i))] = -1
+                dndvi_class[np.where((dndvi_class >= (dndvi_mean - dndvi_std*i)) & (dndvi_class <= (dndvi_mean + dndvi_std*i)))] = 0
+                dndvi_class[np.where(dndvi_class > (dndvi_mean + dndvi_std*i))] = 1
+                
+                name = fileSpot.split('/')[-1].split('.')[0]+'_class_'+str(i)+'sd_dndvi.tif'
+
+                with rasterio.open(os.path.join(pathOutputClass+lineDir, name), 'w', **kwargs) as dst:
+                    dst.write_band(1, dndvi_class.astype(rasterio.int16))
+
+
+    else:
+        continue
 
 
 
